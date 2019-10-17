e21ee4fd4b0ad4fd433cdf73c6f67d11168b0ed8
Soma Sunder <Soma Sunder@DESKTOP-923UG82>
2019-10-18 03:20:15 +0530

    dry

---
+++ shopping-portal/src/main/java/com/som/app/shopping_portal/App.java
@@ -0,0 +1,13 @@
+package com.som.app.shopping_portal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
